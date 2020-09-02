f52afb0a683f28b54ab96dcb666cc2cd7c7009e0
NitzanHersh <NitzanHersh@users.noreply.github.com>
2020-09-02 16:03:52 +0300

    audioManager for music between scenes main, achievements and credits

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    private string stoppingSceneName = "RedShirt_RnM";
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach( Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Start()
+    {
+        Play("MainMenuMusic");
+    }
+
+    public void Update()
+    {
+      if (SceneManager.GetActiveScene().name.Equals(stoppingSceneName))
+        {
+            Stop("MainMenuMusic");
+        }
+    }
+    public void Play (string name)
+    {
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            return;
+        }
+        s.source.Stop();
+    }
+}
