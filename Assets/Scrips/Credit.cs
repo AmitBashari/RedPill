2f5d8698adaa6efd1a05a80fc495e2b560b717ce
AmitBashari <bashari.amit@gmail.com>
2020-09-23 12:47:38 +0300

    Credits Work in proggress

---
+++ Assets/Scrips/Credit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Credit Object")]
+public class Credit : ScriptableObject
+{
+    public string Headline;
+    public string FunnyQuote;
+    public string[] Names;
+}
---
+++ Assets/Scrips/CreditsScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScreen : MonoBehaviour
+{
+    public CreditsEngine CEngine;
+
+    public Text CreditsHeadline;
+    public Text[] CreditsNames;
+    public Text CreditsFunnyQuote;
+
+    public Credit[] CreditObjects;
+
+    public Animator Animator;
+
+    private float letterPause = 0.05f;
+
+    private void Start()
+    {
+        CreditsHeadline.text = CreditObjects[0].Headline;
+        CreditsNames[0].text = CreditObjects[0].Names[0];
+        
+        CreditsNames[1].text = CreditObjects[0].Names[1];
+    }
+}
